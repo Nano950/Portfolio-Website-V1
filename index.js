e1a7def59c882fbef20809a9beff73824d5d4970
Dakota Hanks <dakotahanks1020@gmail.com>
2022-07-26 00:03:04 -0400

    add menu and its functionality

---
+++ index.js
@@ -0,0 +1,9 @@
+function openMobileMenu() {
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("menu-background").style.display = "block";
+}
+
+function closeMobileMenu() {
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("menu-background").style.display = "none";
+}
