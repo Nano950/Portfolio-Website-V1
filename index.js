4409ceff7829cac9148423857c59ed82a2e27bef
Dakota Hanks <dakotahanks1020@gmail.com>
2022-08-13 09:18:58 -0400

    added animations to hero and buttons

--- index.js
+++ index.js
@@ -7,3 +7,24 @@ function closeMobileMenu() {
     document.getElementById("menu").style.display = "none";
     document.getElementById("menu-background").style.display = "none";
 }
+
+let i = 0;
+const txt = 'Dakota Hanks';
+const speed = 50;
+
+
+// Add typerwriter animation to hero text
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("hero-text").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+// set the display of hero text to block so that the animation doesnt play while invisible
+function setBlock() {
+    document.getElementById("hero-text").style.display = "block";
+    typeWriter();
+}
+setTimeout(setBlock, 2000);
